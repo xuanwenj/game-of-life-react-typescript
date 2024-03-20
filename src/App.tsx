89a89e140ae4d0c80798ea214cf068019008c0b5
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-03-20 19:21:35 +1300

    Update the rules of game

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import './App.css';
 const numRows = 50;
 const numCols = 50;
 
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+
+]
 const App: React.FC =() => {
   const [grid, setGrid] = useState(() => {
     const rows = [];
@@ -25,8 +36,32 @@ const App: React.FC =() => {
     if(!runningRef.current) {
       return;
     }
-
-    setTimeout(runSimulation, 1000);
+    //inclue produce to update the state of the grid
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for(let i = 0; i < numRows; i++){
+          for(let j = 0; j < numCols; j++){
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = j + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                  neighbors += g[newI][newK];
+              }
+          });
+          
+          if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+          } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+          }
+    
+          }
+        }
+      })
+    })
+   
+    setTimeout(runSimulation, 250);
   },[]);
 
   return (
@@ -34,6 +69,13 @@ const App: React.FC =() => {
     <button 
       onClick={() => {
         setRunning(!running);
+        //checks whether the simulation was not running before the button was clicked
+          if(!running){
+            runningRef.current = true;
+            runSimulation();
+
+          }
+        
       }}
       >
         {running ? "Stop" : "Start"}
